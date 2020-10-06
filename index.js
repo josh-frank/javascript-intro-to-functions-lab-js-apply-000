872721737d83d614dc478c61987d8ebe9d5e8af1
Joshua Frank <eurosewusa@gmail.com>
2020-10-06 01:17:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ function logShout( string ) { console.log( string.toUpperCase() ); }
 
 function logWhisper( string ) { console.log( string.toLowerCase() ); }
 
+function sayHiToGrandma( string ) {
+  if ( string.toUpperCase() === string ) 
+}
